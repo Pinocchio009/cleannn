2c61b6590c909953b2d0e7c6ebc824fd93e868c3
Pinocchio <officialnikx@gmail.com>
2022-06-06 15:38:02 +0100

    omooo

--- models/user.js
+++ models/user.js
@@ -26,7 +26,7 @@ password: {
 isAdmin: Boolean,
 });
 userSchema.methods.generateAuthToken = function(){
-    const token = jwt.sign({_id: this._id},config.get('jwtPrivateKey'))
+    const token = jwt.sign({_id: this._id, isAdmin: this.isAdmin },config.get('jwtPrivateKey'))
  return token;
 }
 
